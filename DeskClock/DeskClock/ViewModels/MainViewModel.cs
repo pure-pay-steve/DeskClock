cae23c9235db1f370ea55ce14139505a59b670ff
purepay-steve <142355393+purepay-steve@users.noreply.github.com>
2024-06-12 13:10:47 +0200

    Add project files.

---
+++ DeskClock/DeskClock/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ReactiveUI;
+using System;
+using System.Threading.Tasks;
+
+namespace DeskClock.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    
+                    TimeNow = DateTime.Now.ToString("HH:mm:ss");
+                }
+            });
+        }
+
+        private string _timeNow = string.Empty;
+
+        public string TimeNow
+        {
+            get => _timeNow;
+            set => this.RaiseAndSetIfChanged(ref _timeNow, value);
+        }
+    }
+}
